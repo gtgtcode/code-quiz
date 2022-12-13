4150434d25b861a76756c4201d853082ad472094
GeorgeWorkPC <glthomas526@gmail.com>
2022-12-13 10:16:33 -0500

    finished progress bar movement mechanics

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,43 @@
 //Multiplier Progress Bar
 
+
 let progressNumber = document.getElementById("progress-percentage");
 let rootCSS = document.querySelector(':root');
 
-let appendNumber = getComputedStyle(document.documentElement).getPropertyValue('--progress-percentage');
+
+let appendNumber = getComputedStyle(document.documentElement).getPropertyValue("--progress-percentage");
 let percentToNum = parseFloat(appendNumber) / 100.0;
 let outputPercent = percentToNum.toLocaleString("en", {style: "percent"});
 
 rootCSS.style.setProperty('--progress-percentage', outputPercent);
 
-
+progressNumber.innerHTML = outputPercent;
 
 function changePercentage(number) {
-    
     percentToNum += number;
+
+    //Limiting Progress Bar Percentages
+
+    if (percentToNum < 0) {
+        percentToNum = 0;
+    }
+    
+    //1 would be 100% on the progress bar.
+
+    if (percentToNum > 1) {
+        percentToNum = 1;
+    }
+
     outputPercent = percentToNum.toLocaleString("en", {style: "percent"});
     rootCSS.style.setProperty('--progress-percentage', outputPercent);
 
-    progressNumber.append(outputPercent);
+    progressNumber.innerHTML = outputPercent;
     
-    return outputPercent, progressNumber;
 }
 
-progressNumber.append(outputPercent);
+
+
+
 
 
 
