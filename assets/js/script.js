d38cd64fbe62ba9f645c3ef6f0967a527a092018
George <glthomas526@gmail.com>
2022-12-12 22:00:45 -0500

    started work on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-document.getElementById
+//Multiplier Progress Bar
+
+let progressNumber = document.getElementById("progress-percentage");
+let rootCSS = document.querySelector(':root');
+
+let appendNumber = getComputedStyle(document.documentElement).getPropertyValue('--progress-percentage');
+let percentToNum = parseFloat(appendNumber) / 100.0;
+let outputPercent = percentToNum.toLocaleString("en", {style: "percent"});
+
+rootCSS.style.setProperty('--progress-percentage', outputPercent);
+
+
+
+function changePercentage(number) {
+    
+    percentToNum += number;
+    outputPercent = percentToNum.toLocaleString("en", {style: "percent"});
+    rootCSS.style.setProperty('--progress-percentage', outputPercent);
+
+    progressNumber.append(outputPercent);
+    
+    return outputPercent, progressNumber;
+}
+
+progressNumber.append(outputPercent);
+
+
+
+
+
